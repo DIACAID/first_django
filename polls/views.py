4e04b02a43d089b0437543f6560bc92c063f3e94
DIACAID <tome.its02@gmail.com>
2025-09-03 14:06:15 -0400

    feat(templates/polls/index.html): añadiendo vista para index

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from django.template import loader
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello world. You're the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def details(request, question_id):
     return HttpResponse("Youre looking at question %s" % question_id)
