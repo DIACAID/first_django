82dd5df083848751c63551b17323d0b73392da5b
DIACAID <tome.its02@gmail.com>
2025-09-03 10:48:37 -0400

    feat(views.py): vistas de prueba restantes

--- polls/views.py
+++ polls/views.py
@@ -5,5 +5,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello world. You're the polls index")
 
+def details(request, question_id):
+    return HttpResponse("Youre looking at question %s" % question_id)
+
 def results(request, question_id):
+    response = "Youre looking at question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
     return HttpResponse("Youre looking at question %s" % question_id)
